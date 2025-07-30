cff59a24fdc7811e10b25c4a3c93aa9517ce9cc2
tokebe <43009413+tokebe@users.noreply.github.com>
2025-07-30 17:00:51 -0400

    allow Sentry spans without OTel collector

--- src/retriever/utils/telemetry.py
+++ src/retriever/utils/telemetry.py
@@ -10,7 +10,7 @@ from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor
 from opentelemetry.propagate import set_global_textmap
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, SpanExporter
 from sentry_sdk.integrations.opentelemetry import SentryPropagator, SentrySpanProcessor
 
 from retriever.config.general import CONFIG
@@ -30,21 +30,26 @@ def configure_telemetry(app: FastAPI | None = None) -> None:
         )
 
     if any([CONFIG.telemetry.sentry_enabled, CONFIG.telemetry.otel_enabled]):
-        if CONFIG.telemetry.otel_host is None:
-            raise ValueError("otel_host must be set if otel is enabled.")
-        collector_address = f"http://{CONFIG.telemetry.otel_host.get_secret_value()}:{CONFIG.telemetry.otel_port}"
+        collector_address = ""
+        if CONFIG.telemetry.otel_host:
+            collector_address = f"http://{CONFIG.telemetry.otel_host.get_secret_value()}:{CONFIG.telemetry.otel_port}"
 
-        if parent_process() is None:
-            log.info(f"Telemetry enabled, settings up service for {collector_address}")
+            if parent_process() is None:
+                log.info(
+                    f"Telemetry enabled, settings up service for {collector_address}"
+                )
 
         # Service name is required for most backends
         resource = Resource(attributes={SERVICE_NAME: "retriever"})
         trace_provider = TracerProvider(resource=resource)
         trace_provider.add_span_processor(SentrySpanProcessor())
+        # Allows Sentry to still process spans even if there is no OTel collector
         processor = BatchSpanProcessor(
             OTLPSpanExporter(
                 endpoint=f"{collector_address}{CONFIG.telemetry.otel_trace_endpoint}"
             )
+            if CONFIG.telemetry.otel_enabled
+            else SpanExporter()  # essentially a NoOp exporter
         )
         trace_provider.add_span_processor(processor)
         trace.set_tracer_provider(trace_provider)
