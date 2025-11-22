948baf85bf37769270cc856e51fca7e3ac83179f
Everaldo <everaldorodrigo@gmail.com>
2025-11-21 16:45:26 -0800

    fix(dgraph): lint.

--- src/retriever/data_tiers/tier_0/dgraph/query.py
+++ src/retriever/data_tiers/tier_0/dgraph/query.py
@@ -24,7 +24,9 @@ class DgraphQuery(Tier0Query):
         query_payload = transpiler.process_qgraph(qgraph)
         self.job_log.trace(query_payload)
 
-        backend_record = await backend_driver.run_query(query_payload, transpiler=transpiler)
+        backend_record = await backend_driver.run_query(
+            query_payload, transpiler=transpiler
+        )
 
         # Convert neo4j record response to TRAPI
         with tracer.start_as_current_span("transform_results"):
