8b3e32c577841f784979f9b5fa4e07d1118fc929
ctrl-schaff <schaffjohnathan@gmail.com>
2025-12-08 14:21:08 -0600

    Add initial tests for set_interpretation support

---
+++ tests/set_interpretation/conftest.py
@@ -0,0 +1,108 @@
+"""
+Collection of fixtures for testing the set_interpretation handling
+"""
+
+import pytest
+
+from retriever.types.trapi import (
+    CURIE,
+    BiolinkEntity,
+    BiolinkPredicate,
+    MessageDict,
+    ParametersDict,
+    QEdgeDict,
+    QEdgeID,
+    QNodeDict,
+    QNodeID,
+    QueryDict,
+    QueryGraphDict,
+)
+from reasoner_pydantic import Query
+
+
+@pytest.fixture(scope="session")
+def set_interpretation_batch_query() -> dict:
+    query = QueryDict(
+        parameters=ParametersDict(tiers=[0, 1]),
+        submitter="setinterp-automated-batch-testing",
+        message=MessageDict(
+            query_graph=QueryGraphDict(
+                nodes={
+                    QNodeID("n0"): QNodeDict(
+                        ids=[CURIE("NCBIGene:3778")],
+                        categories=[BiolinkEntity("biolink:Gene")],
+                        set_interpretation="BATCH",
+                        constraints=[],
+                        member_ids=[],
+                    ),
+                    QNodeID("n1"): QNodeDict(
+                        ids=None,
+                        categories=[BiolinkEntity("biolink:Disease")],
+                        set_interpretation="BATCH",
+                        constraints=[],
+                        member_ids=[],
+                    ),
+                },
+                edges={
+                    QEdgeID("e01"): QEdgeDict(
+                        subject=QNodeID("n0"),
+                        object=QNodeID("n1"),
+                        predicates=[BiolinkPredicate("biolink:causes")],
+                        attribute_contraints=[],
+                        qualifier_constraints=[],
+                    ),
+                },
+            )
+        ),
+    )
+    query = Query.model_validate(query)
+    return query
+
+
+@pytest.fixture(scope="session")
+def mock_query_result():
+    results = [
+        {
+            "node_bindings": {
+                "n1": [{"id": "MONDO:0012276", "attributes": []}],
+                "n0": [{"id": "NCBIGene:3778", "attributes": []}],
+            },
+            "analyses": [
+                {
+                    "resource_id": "infores:retriever",
+                    "edge_bindings": {
+                        "e01": [{"id": "ce645c286b2f", "attributes": []}]
+                    },
+                }
+            ],
+        },
+        {
+            "node_bindings": {
+                "n1": [{"id": "MONDO:0060551", "attributes": []}],
+                "n0": [{"id": "NCBIGene:3778", "attributes": []}],
+            },
+            "analyses": [
+                {
+                    "resource_id": "infores:retriever",
+                    "edge_bindings": {
+                        "e01": [{"id": "b882c438207a", "attributes": []}]
+                    },
+                }
+            ],
+        },
+        {
+            "node_bindings": {
+                "n1": [{"id": "MONDO:0032886", "attributes": []}],
+                "n0": [{"id": "NCBIGene:3778", "attributes": []}],
+            },
+            "analyses": [
+                {
+                    "resource_id": "infores:retriever",
+                    "edge_bindings": {
+                        "e01": [{"id": "3a3380222bd5", "attributes": []}]
+                    },
+                }
+            ],
+        },
+    ]
+    return results
