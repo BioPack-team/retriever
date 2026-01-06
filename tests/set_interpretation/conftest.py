deb8077f1a85c036e75c28e5e756f9f7b4f52ea0
ctrl-schaff <schaffjohnathan@gmail.com>
2026-01-06 00:00:14 -0600

    Add tests for malformed queries

--- tests/set_interpretation/conftest.py
+++ tests/set_interpretation/conftest.py
@@ -31,6 +31,8 @@ class MockQuery:
 
 
 # --- BATCH SET INTERPRETATION QUERIES ---
+
+
 @pytest.fixture(scope="session")
 def mock_batch_query() -> MockQuery:
     query = QueryDict(
@@ -1353,6 +1355,178 @@ def mock_mixed_query5() -> dict:
                     {"id": ["7c40623f-9da9-5aeb-985d-0d7428dd76ae"], "attributes": []}
                 ],
                 "n1": [{"id": "MONDO:0000001", "attributes": []}],
+            },
+            "analyses": [
+                {
+                    "resource_id": "infores:retriever",
+                    "edge_bindings": {
+                        "e0": [
+                            {"id": "7f730935b4f8", "attributes": []},
+                            {"id": "7c2a3a2bb437", "attributes": []},
+                            {"id": "7b6641969611", "attributes": []},
+                        ]
+                    },
+                }
+            ],
+        },
+    ]
+
+    query = Query.model_validate(query)
+
+    return MockQuery(
+        query.message.query_graph.model_dump(), prefilter_results, postfilter_results
+    )
+
+
+# --- MALFORMED SET INTERPRETATION QUERIES ---
+
+
+@pytest.fixture(scope="session")
+def mock_malformed_query() -> dict:
+    """Represents a fully connected set of results.
+
+    This query is manipulated at runtime for tests where
+    we wish to verify how we handle malformed queries
+    """
+    query = QueryDict(
+        parameters=ParametersDict(tiers=[0, 1]),
+        submitter="setinterp-automated-testing",
+        message=MessageDict(
+            query_graph=QueryGraphDict(
+                nodes={
+                    QNodeID("n0"): QNodeDict(
+                        ids=[CURIE("MONDO:0008903"), CURIE("MONDO:0000001")],
+                        set_interpretation="BATCH",
+                        constraints=[],
+                    ),
+                    QNodeID("n1"): QNodeDict(
+                        ids=[
+                            str(uuid.UUID("uuid:7c40623f-9da9-5aeb-985d-0d7428dd76ae"))
+                        ],
+                        set_interpretation="ALL",
+                        constraints=[],
+                        member_ids=[
+                            CURIE("MONDO:0000532"),
+                            CURIE("UMLS:C2983716"),
+                            CURIE("MONDO:0020644"),
+                        ],
+                    ),
+                },
+                edges={
+                    QEdgeID("e01"): QEdgeDict(
+                        subject=QNodeID("n0"),
+                        object=QNodeID("n1"),
+                        predicates=[BiolinkPredicate("biolink:subclass_of")],
+                        attribute_contraints=[],
+                        qualifier_constraints=[],
+                    ),
+                },
+            )
+        ),
+    )
+
+    prefilter_results = [
+        {
+            "node_bindings": {
+                "n1": [{"id": "MONDO:0000532", "attributes": []}],
+                "n0": [{"id": "MONDO:0000001", "attributes": []}],
+            },
+            "analyses": [
+                {
+                    "resource_id": "infores:retriever",
+                    "edge_bindings": {"e0": [{"id": "7f730935b4f8", "attributes": []}]},
+                }
+            ],
+        },
+        {
+            "node_bindings": {
+                "n1": [{"id": "MONDO:0020644", "attributes": []}],
+                "n0": [{"id": "MONDO:0000001", "attributes": []}],
+            },
+            "analyses": [
+                {
+                    "resource_id": "infores:retriever",
+                    "edge_bindings": {"e0": [{"id": "7c2a3a2bb437", "attributes": []}]},
+                }
+            ],
+        },
+        {
+            "node_bindings": {
+                "n1": [{"id": "UMLS:C2983716", "attributes": []}],
+                "n0": [{"id": "MONDO:0000001", "attributes": []}],
+            },
+            "analyses": [
+                {
+                    "resource_id": "infores:retriever",
+                    "edge_bindings": {"e0": [{"id": "7b6641969611", "attributes": []}]},
+                }
+            ],
+        },
+        {
+            "node_bindings": {
+                "n1": [{"id": "MONDO:0020644", "attributes": []}],
+                "n0": [{"id": "MONDO:0008903", "attributes": []}],
+            },
+            "analyses": [
+                {
+                    "resource_id": "infores:retriever",
+                    "edge_bindings": {"e0": [{"id": "0b6c704dfd94", "attributes": []}]},
+                }
+            ],
+        },
+        {
+            "node_bindings": {
+                "n1": [{"id": "UMLS:C2983716", "attributes": []}],
+                "n0": [{"id": "MONDO:0008903", "attributes": []}],
+            },
+            "analyses": [
+                {
+                    "resource_id": "infores:retriever",
+                    "edge_bindings": {"e0": [{"id": "c8255a314650", "attributes": []}]},
+                }
+            ],
+        },
+        {
+            "node_bindings": {
+                "n1": [{"id": "MONDO:0000532", "attributes": []}],
+                "n0": [{"id": "MONDO:0008903", "attributes": []}],
+            },
+            "analyses": [
+                {
+                    "resource_id": "infores:retriever",
+                    "edge_bindings": {"e0": [{"id": "6b2e82827546", "attributes": []}]},
+                }
+            ],
+        },
+    ]
+
+    postfilter_results = [
+        {
+            "node_bindings": {
+                "n0": [{"id": "MONDO:0008903", "attributes": []}],
+                "n1": [
+                    {"id": ["7c40623f-9da9-5aeb-985d-0d7428dd76ae"], "attributes": []}
+                ],
+            },
+            "analyses": [
+                {
+                    "resource_id": "infores:retriever",
+                    "edge_bindings": {
+                        "e0": [
+                            {"id": "6b2e82827546", "attributes": []},
+                            {"id": "0b6c704dfd94", "attributes": []},
+                            {"id": "c8255a314650", "attributes": []},
+                        ]
+                    },
+                }
+            ],
+        },
+        {
+            "node_bindings": {
+                "n0": [{"id": "MONDO:0000001", "attributes": []}],
+                "n1": [
+                    {"id": ["7c40623f-9da9-5aeb-985d-0d7428dd76ae"], "attributes": []}
+                ],
             },
             "analyses": [
                 {
