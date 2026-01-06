3921ccc9271d5579aa8cb42320661be1d9f12ca9
ctrl-schaff <schaffjohnathan@gmail.com>
2026-01-06 00:00:14 -0600

    Add tests for malformed queries

--- tests/set_interpretation/test_set_interpretation.py
+++ tests/set_interpretation/test_set_interpretation.py
@@ -86,3 +86,112 @@ async def test_mixed_set_interpretation_values(
                 == expected_result["node_bindings"]["n1"][0]["id"]
             ):
                 assert empirical_result == expected_result
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "mock_query",
+    [
+        "mock_malformed_query",
+    ],
+)
+async def test_malformed_set_identifier(
+    mock_query: str, request: _pytest.fixtures.TopRequest
+):
+    """Test case where the set identifier isn't a valid UUID."""
+    mock_malformed_query: MockQuery = request.getfixturevalue(mock_query)
+
+    # Ensure that the nodes have two different values for set_interpretation
+    job_log = TRAPILogger(job_id=random.randint(0, 10000))
+
+    # Set the ids field for node n1 to something other than a UUID
+    mock_malformed_query.query["nodes"]["n1"]["ids"] = ["woodcarving"]
+
+    empirical_results0 = evaluate_set_interpretation(
+        qgraph=mock_malformed_query.query,
+        results=mock_malformed_query.prefilter_results,
+        job_log=job_log,
+    )
+
+    assert len(empirical_results0) != len(mock_malformed_query.postfilter_results)
+
+    # Set the ids field for node n1 to an empty list
+    mock_malformed_query.query["nodes"]["n1"]["ids"] = []
+
+    empirical_results1 = evaluate_set_interpretation(
+        qgraph=mock_malformed_query.query,
+        results=mock_malformed_query.prefilter_results,
+        job_log=job_log,
+    )
+
+    assert len(empirical_results1) != len(mock_malformed_query.postfilter_results)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "mock_query",
+    [
+        "mock_malformed_query",
+    ],
+)
+async def test_malformed_set_interpretation_value(
+    mock_query: str, request: _pytest.fixtures.TopRequest
+):
+    """Test case where the set_interpretation value isn't BATCH|ALL|MANY."""
+    mock_malformed_query: MockQuery = request.getfixturevalue(mock_query)
+
+    # Ensure that the nodes have two different values for set_interpretation
+    job_log = TRAPILogger(job_id=random.randint(0, 10000))
+
+    # Set the set_interpretation value to an invalid value
+    mock_malformed_query.query["nodes"]["n0"]["set_interpretation"] = "SIGNAL"
+    mock_malformed_query.query["nodes"]["n1"]["set_interpretation"] = "IRONCLAD"
+
+    empirical_results = evaluate_set_interpretation(
+        qgraph=mock_malformed_query.query,
+        results=mock_malformed_query.prefilter_results,
+        job_log=job_log,
+    )
+
+    assert len(empirical_results) != len(mock_malformed_query.postfilter_results)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "mock_query",
+    [
+        "mock_malformed_query",
+    ],
+)
+async def test_malformed_member_identifiers(
+    mock_query: str, request: _pytest.fixtures.TopRequest
+):
+    """Test case where the member_ids attribute is malformed."""
+    mock_malformed_query: MockQuery = request.getfixturevalue(mock_query)
+
+    # Ensure that the nodes have two different values for set_interpretation
+    job_log = TRAPILogger(job_id=random.randint(0, 10000))
+
+    # Set the member_ids value to a malformed entry
+    mock_malformed_query.query["nodes"]["n1"]["set_interpretation"] = "ALL"
+    mock_malformed_query.query["nodes"]["n1"]["member_ids"] = []
+
+    empirical_results = evaluate_set_interpretation(
+        qgraph=mock_malformed_query.query,
+        results=mock_malformed_query.prefilter_results,
+        job_log=job_log,
+    )
+
+    assert len(empirical_results) != len(mock_malformed_query.postfilter_results)
+
+    # Set the member_ids value to a malformed entry
+    mock_malformed_query.query["nodes"]["n1"]["set_interpretation"] = "MANY"
+    mock_malformed_query.query["nodes"]["n1"]["member_ids"] = None
+
+    empirical_results = evaluate_set_interpretation(
+        qgraph=mock_malformed_query.query,
+        results=mock_malformed_query.prefilter_results,
+        job_log=job_log,
+    )
+
+    assert len(empirical_results) != len(mock_malformed_query.postfilter_results)
